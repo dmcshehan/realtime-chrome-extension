fe798ae395abdfb46693cc6f02eab0519a11e95d
Shehan <dmcshehan@outlook.com>
2020-11-23 18:52:20 +0530

    notification

--- background.js
+++ background.js
@@ -1,9 +1,25 @@
 /* Connects to the socket server */
 
-var socket = io.connect('https://real-time-ce-back.herokuapp.com/');
+//var socket = io.connect('https://real-time-ce-back.herokuapp.com/');
+var socket = io.connect('http://localhost:5000/');
 
 socket.on('connect', function () {
     console.log('Client successfully connected to server!');
+});
+
+
+socket.on('reply', function (message) {
+    console.log(message);
+
+    const notifObject = {
+        type: 'basic',
+        iconUrl: './icon64.png',
+        title: 'Someone Poked You',
+        message: message
+    };
+
+    chrome.notifications.create('notification', notifObject);
+
 });
 
 
