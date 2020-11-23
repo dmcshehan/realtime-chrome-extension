853a870d860358985c6dad95a56786c69867252e
Shehan <dmcshehan@outlook.com>
2020-11-23 18:17:42 +0530

    room

--- background.js
+++ background.js
@@ -3,16 +3,12 @@
 var socket = io.connect('https://real-time-ce-back.herokuapp.com/');
 
 socket.on('connect', function () {
-    socket.join('pork-room');
-
-    socket.on('pork', message => {
-        console.log(message);
-    })
+    console.log('Client successfully connected to server!');
 });
 
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     if (request.todo == 'poked') {
-        socket.to('pork-room').emit('pork', 'dmcshehan');
+        socket.emit('pork', 'I pork you');
     }
 })
